f04210598766f49a5368e5c3fb1f8df3f8ab5ea2
Balatoni István <balatoniistvan@stud.uni-obuda.hu>
2025-02-17 13:09:37 +0100

    Fix Program.cs

--- Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
+++ Taxually.TechnicalTest/Taxually.TechnicalTest/Program.cs
@@ -6,7 +6,6 @@ using Taxually.TechnicalTest.Handlers.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 // Register Clients
